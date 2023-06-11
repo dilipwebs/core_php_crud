ca0481d7452572968514d84bb0a23a9fd8dfbf2b
Dilip Webs <dilipwebs@gmail.com>
2023-06-11 20:14:11 +0530

    registration and db connection

--- db_con.php
+++ db_con.php
@@ -3,3 +3,14 @@
 $servername = 'localhost';
 $username = 'root';
 $password = '';
+$db = 'core_php_crud';
+
+mysqli_report(MYSQLI_REPORT_STRICT);
+
+try {
+    $con = new mysqli($servername, $username, $password, $db);
+    date_default_timezone_set('Asia/Kolkata');
+} catch (Exception $ex) {
+    echo "Connection failed: ".$ex->getMessage();
+    exit();
+}
